c72b475121f43df20e5109970151f0b01311e3be
Rob Garner <rgarner011235@gmail.com>
2020-12-02 18:06:43 -0700

    Add project files.

---
+++ EmailDemonstration/Server/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailDemonstration.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpGet]
+        public string GetEmail()
+        {
+            return "Hello from email controller";
+        }
+
+        [HttpPost]
+        public IActionResult PostEmail(Email )
+    }
+}
---
+++ EmailDemonstration/Shared/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailDemonstration.Shared
+{
+    class Email
+    {
+        public string Address { get; set; }
+        public string ToName { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }        
+    }
+}
